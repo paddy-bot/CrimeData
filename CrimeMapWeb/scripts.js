042665792104010d4d2cd314346de5b449363579
HeinrichRausch <105247316+HeinrichRausch@users.noreply.github.com>
2023-04-13 15:25:04 -0500

    Added code to hide the navbar when the arrow is clicked

--- CrimeMapWeb/scripts.js
+++ CrimeMapWeb/scripts.js
@@ -3,7 +3,24 @@ const navArrow = document.querySelector('.arrow');
 const navMenu = document.querySelector('.nav-menu');
 
 navArrow.addEventListener("click", () => {
-    navArrow.classList.remove("right");
-    navArrow.classList.add("left");
-    
+	hideNav();
 });
+
+
+function hideNav() { //Hides the Nav
+	
+	if (document.getElementById("hideNav1").style.display == 'flex') {
+		document.getElementById("hideNav1").style.display = "none";
+		document.getElementById("hideNav2").style.display = "none";
+		document.getElementById("hideNav3").style.display = "none";
+		navArrow.classList.remove("right");
+    	navArrow.classList.add("left");
+	} else {
+		document.getElementById("hideNav1").style.display = "flex";
+		document.getElementById("hideNav2").style.display = "flex";
+		document.getElementById("hideNav3").style.display = "flex";
+    	navArrow.classList.remove("left");
+    	navArrow.classList.add("right");
+	}
+	
+};
