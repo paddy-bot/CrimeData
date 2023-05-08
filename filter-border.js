074ecbfe95c02b8c87bd889b8eba7aa71f7dfa07
Paddy McGuire <mcguirepd@hotmail.com>
2023-05-08 14:40:04 -0500

    temp buttons

--- filter-border.js
+++ filter-border.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".crime-filter-item[data-color]").forEach(function (item) {
     var borderColor = item.getAttribute("data-color");
-    item.style.borderColor = borderColor;
-    item.style.borderWidth = "4px";
-    item.style.borderStyle = "outset";
+    item.style.backgroundColor = borderColor;
   });
 });
